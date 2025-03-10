34453f3371f4cef12070f7ea09821cd176b552d7
faukirijatul <faukirijatul42@gmail.com>
2025-03-10 11:06:45 +0700

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Counter from "./components/Counter";
 import Todo from "./components/Todo";
 import Meal from "./components/Meal";
+import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <Todo /> */}
-      <Meal />
+      {/* <Meal /> */}
+      <Calculator />
     </div>
   );
 };
